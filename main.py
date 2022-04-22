86413d5328b2c1e043a8658b810a198b4ab2fecb
alinivan <ai626@live.mdx.ac.uk>
2022-04-22 18:05:27 +0100

    added xmlrpc server

--- main.py
+++ main.py
@@ -4,6 +4,35 @@ from core import requestEmptyTin
 from core import requestSealValidation
 from core import requestRFIDValidation
 
-print(requestEmptyTin())
-print(requestSealValidation())
-print(requestRFIDValidation())
+import sys
+import urlib
+
+is_py2 = sys.version[0] == '2'
+if is_py2:
+  from SimpleXMLRPCServer import SimpleXMLRPCServer
+else:
+  from xmlrpc.server import SimpleXMLRPCServer
+
+def get_next_pose():
+    #assert type(p) is dict
+    #pose = urlib.poseToList(p)
+    #print("Received pose: " + str(pose))
+    #pose = [-0.18, -0.61, 0.23, 0, 3.12, 0.04];
+    #return urlib.listToPose(pose);
+    return "Walabila!"    
+    
+server = SimpleXMLRPCServer(("localhost", 50000))
+server.RequestHandlerClass.protocol_version = "HTTP/1.1"
+print("Listening on port 50000...")
+
+server.register_function(get_next_pose, "get_next_pose")
+server.register_function(requestEmptyTin, "requestEmptyTin")
+server.register_function(requestSealValidation, "requestSealValidation")
+server.register_function(requestRFIDValidation, "requestRFIDValidation")
+
+server.serve_forever()
+
+
+#print(requestEmptyTin())
+#print(requestSealValidation())
+#print(requestRFIDValidation())
